2612403ceb0a0523ab2f59a1c8dbe32ff868322f
Sasa-Bose <95925074+Sasa-Bose@users.noreply.github.com>
2024-03-18 18:11:53 +0530

    Add files via upload
    Spark job is written here, then we have to open terminal and go to this py file location and execute spark-submit file.py
    The output will get stored in the output location provided.
    To know how to query the output file see Notebook_output_analysis.

---
+++ spark_job.py
@@ -0,0 +1,43 @@
+# %% [markdown]
+# ## Data discovery: Load and query Yellow Taxi data
+# > Download the dataset from [the official TLC Trip Record Data website](https://www.nyc.gov/site/tlc/about/tlc-trip-record-data.page)
+from pyspark.sql import SparkSession
+
+# %%
+# Create SparkSession
+spark = SparkSession.builder\
+             .master("local[1]")\
+             .appName("spark-app-version-x")\
+             .getOrCreate()
+
+# %%
+# Read taxi data. These can be input parameters, using python arguments.
+input_path = '/home/sasa/Downloads/Code/spark_jobs/production_data/yellow_taxis'
+output_path = '/home/sasa/Downloads/Code/spark_jobs/reporting/etl_job_taxis_multi_passanger_trips/'
+
+
+# %%
+# Load data into Spark dataframe
+df = spark.read.parquet(input_path)
+
+# %%
+# Query sample, using Spark SQL
+df.createOrReplaceTempView('tbl_raw_yellow_taxis')
+
+# %%
+# SQL Statement
+df_output = spark.sql('''
+                        SELECT *
+                        FROM tbl_raw_yellow_taxis 
+                        ''')
+
+# %%
+# Write data to output
+df_output.write.mode('overwrite').parquet(output_path)
+
+# %%
+# Stop the session
+spark.stop()
+
+
+
